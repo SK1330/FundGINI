1e4aba169d1075b100c4d59f99293b7cb4f95073
Saurabh Kavhar <76881730+SK1330@users.noreply.github.com>
2021-12-19 19:15:07 +0530

    Add files via upload

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
     $('.first-slider').owlCarousel({
+        // nav: true,
         loop: true,
-        nav: true,
         autoplay: true,
-        // autoplayTimeout: 1000,
-        navText: ["<i class='fas fa-angle-left'></i>", "<i class='fas fa-angle-right'></i>"],
+        autoplayTimeout: 1000,
+        // navText: ["<i class='fas fa-angle-left'></i>", "<i class='fas fa-angle-right'></i>"],
         center: true,
         responsive: {
             320: {
                 items: 1,
-                nav: false,
+                // nav: false,
             },
             576: {
                 items: 2,
-                nav: true,
+                // nav: true,
             },
             1350: {
                 items: 4,
-                nav: true,
+                // nav: true,
             }
 
         }
     });
 });
+
+
+
 
 
 $(document).ready(function() {
